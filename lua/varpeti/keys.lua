a3b1d06fa0ee1d208de35550275ab687d5d7b382
varpeti <peti.var97@gmail.com>
2023-01-03 17:27:25 +0100

    Telescope keys

--- lua/varpeti/keys.lua
+++ lua/varpeti/keys.lua
@@ -1,16 +1,22 @@
 vim.g.mapleader = " "
 
 local wk = require("which-key")
+local tele = require("telescope.builtin")
+local config_dir = "~/.config/nvim"
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  config_dir = os.getenv("HOME") .. "\\AppData\\Local\\nvim"
+end
 
 wk.register({
   ["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle current line" },
   f = {
     name = "Files",
-    f = { "<cmd>Telescope find_files<cr>", "Find File" },
-    g = { "<cmd>Telescope live_grep<cr>", "Grep" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+    f = { tele.find_files, "Find File" },
+    g = { tele.live_grep, "Grep" },
+    s = { tele.grep_string, "Grep String under word" },
+    r = { tele.oldfiles, "Open Recent File" },
     n = { "<cmd>enew<cr>", "New File" },
-    o = { "<cmd>NvimTreeOpen ~/.config/nvim<cr>", "Config folder"},
+    o = { "<cmd>NvimTreeOpen "..config_dir.."<cr>", "Config folder" },
   },
   h = { "<cmd>nohlsearch<CR>", "No Highlight" },
   e = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
@@ -55,7 +61,6 @@ wk.register({
     name = "Packer",
     c = { "<cmd>PackerCompile<cr>", "Compile" },
     i = { "<cmd>PackerInstall<cr>", "Install" },
-    r = { "<cmd>lua require('lvim.plugin-loader').recompile()<cr>", "Re-compile" },
     s = { "<cmd>PackerSync<cr>", "Sync" },
     S = { "<cmd>PackerStatus<cr>", "Status" },
     u = { "<cmd>PackerUpdate<cr>", "Update" },
@@ -70,7 +75,7 @@ wk.register({
   -- " Debugging
   g = {
     name = "Git TODO",
-    g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
+    g = { "<cmd>TODO lua require '???.terminal'.lazygit_toggle()<cr>", "Lazygit" },
     j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
     k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
     l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
@@ -82,11 +87,11 @@ wk.register({
       "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
       "Undo Stage Hunk",
     },
-    o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+    o = { tele.git_status, "Open changed file" },
+    b = { tele.git_branches, "Checkout branch" },
+    c = { tele.git_commits, "Checkout commit" },
     C = {
-      "<cmd>Telescope git_bcommits<cr>",
+      tele.git_bcommits,
       "Checkout commit(for current file)",
     },
     d = {
@@ -98,7 +103,7 @@ wk.register({
     name = "LSP",
     a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
     d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
-    w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+    w = { tele.diagnostics, "Diagnostics" },
     f = { vim.lsp.buf.format, "Format" },
     i = { "<cmd>LspInfo<cr>", "Info" },
     I = { "<cmd>Mason<cr>", "Mason Info" },
@@ -113,28 +118,28 @@ wk.register({
     l = { vim.lsp.codelens.run, "CodeLens Action" },
     q = { vim.diagnostic.setloclist, "Quickfix" },
     r = { vim.lsp.buf.rename, "Rename" },
-    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+    s = { tele.lsp_document_symbols, "Document Symbols" },
     S = {
-      "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+      tele.lsp_dynamic_workspace_symbols,
       "Workspace Symbols",
     },
-    e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+    e = { tele.quickfix, "Telescope Quickfix" },
   },
   s = {
     name = "Search",
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-    f = { "<cmd>Telescope find_files<cr>", "Find File" },
-    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
-    H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
-    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-    R = { "<cmd>Telescope registers<cr>", "Registers" },
-    t = { "<cmd>Telescope live_grep<cr>", "Text" },
-    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-    C = { "<cmd>Telescope commands<cr>", "Commands" },
+    b = { tele.git_branches, "Checkout branch" },
+    c = { tele.colorscheme, "Colorscheme" },
+    f = { tele.find_files, "Find File" },
+    h = { tele.help_tags, "Find Help" },
+    H = { tele.highlights, "Find highlight groups" },
+    M = { tele.man_pages, "Man Pages" },
+    r = { tele.oldfiles, "Open Recent File" },
+    R = { tele.registers, "Registers" },
+    t = { tele.live_grep, "Text" },
+    k = { tele.keymaps, "Keymaps" },
+    C = { tele.commands, "Commands" },
     p = {
-      "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
+      function() tele.colorscheme({ enable_preview = true }) end,
       "Colorscheme with Preview",
     },
   },
